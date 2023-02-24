b1eb6eeb7f6907e8e872f694c0f5356a5d91b2c8
jusdepatate <jusdepatate@protonmail.com>
2023-02-24 09:50:04 +0100

    use router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './pages/style/main.css';
-
 import Shop from './pages/Shop';
+import Login from "./pages/Login";
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Footer from "./components/Footer";
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <Header />
-    <NavBar />
-    <Shop />
-    <Footer />
+      <Header />
+      <NavBar />
+      
+      <Router>
+          <Routes>
+              <Route exact path="/" element={<Shop />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+      </Router>
+
+      <Footer />
   </React.StrictMode>
 );
 
